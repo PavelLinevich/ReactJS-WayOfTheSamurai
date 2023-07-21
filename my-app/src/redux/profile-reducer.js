2235ca4fcbac8843a542a57eb55aeb308c1a6833
PavelGith <linevich.p.l@gmail.com>
2023-07-21 11:27:41 +0300

    feat: redux

--- my-app/src/redux/profile-reducer.js
+++ my-app/src/redux/profile-reducer.js
@@ -1,7 +1,15 @@
 const ADD_POST = "ADD-POST";
 const UPDATE_NEW_POST_TEXT = "UPDATE-NEW-POST-TEXT";
 
-const profileReducer = (state, action) => {
+let initialState = {
+  posts: [
+    { id: 1, message: "Hello. How are you?", like: 2 },
+    { id: 2, message: "HI! I am fine!", like: 5 },
+  ],
+  newPostText: "",
+}
+
+const profileReducer = (state = initialState, action) => {
   switch (action.type) {
     case ADD_POST:
       let newPost = {
